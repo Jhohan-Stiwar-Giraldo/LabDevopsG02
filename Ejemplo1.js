1ab35ae9c3613577b084ca5f73d75113cd6a8aee
stiwar <stiliguar85@gmail.com>
2023-11-27 17:21:01 -0500

    Segundo Commit Enfoque empresarial (Spam)

--- Ejemplo1.js
+++ Ejemplo1.js
@@ -1,4 +1,8 @@
-var micarro={
-    marca: "renault",
-    color:"azul",
+var miEmpresa={
+    Nombre: "Divals_Electronics",
+    Mercado:"Impresiones 3d y desarrollo de software",
+    Since: "2023",
+    Empleados: 2,
+    Director: "Jhohan Stiwar Giraldo",
+    Diseñadora: "Valentina Herrera",
 }
